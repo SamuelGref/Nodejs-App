9c25eb675741e3087915b796353e8b52110411e4
Samuel Gref <samgref@gmail.com>
2021-12-22 23:28:47 -0500

    refactor

--- complete-node-bootcamp-master/4-natours/app.js
+++ complete-node-bootcamp-master/4-natours/app.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello from the  middleware');
+  next();
+});
+
 const getAlltours = (req, res) => {
   res.status(200).json({
     satutus: 'success',
