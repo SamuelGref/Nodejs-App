ee68d58c6e087d8822937a2170f7a8e15d50617b
Samuel Gref <samgref@gmail.com>
2021-12-22 20:38:20 -0500

    Post

---
+++ complete-node-bootcamp-master/4-natours/app.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'hello from the server', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).send('you can post to this endpointe');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    satutus: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  res.status(200).json({
+    satutus: 'success',
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App running on prot ${port}....`);
+});
